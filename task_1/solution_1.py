6fe52c2c546f4d27a988041a080aa1213f92af8b
Cultrett <72413163+Cultrett@users.noreply.github.com>
2020-10-22 14:22:39 +0300

    Update solution_1.py

--- task_1/solution_1.py
+++ task_1/solution_1.py
@@ -1,1 +1,8 @@
-# your code here
+#1.sorting the list in ascending order and printing the last element:
+mylist = [3,5,0,7,9]
+mylist.sort()
+print("largest element is:", mylist[-1])
+
+#2.using (max):
+mylist= [3,5,0,7,9]
+print("largest element is:", max(mylist))
