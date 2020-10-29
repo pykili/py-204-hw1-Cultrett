440d6dccf7eb7a3a4ea71450199f23edd58cc5cd
Cultrett <72413163+Cultrett@users.noreply.github.com>
2020-10-29 22:33:00 +0300

    Update solution_1.py

--- task_1/solution_1.py
+++ task_1/solution_1.py
@@ -6,3 +6,12 @@ print("largest element is:", mylist[-1])
 #2.using (max):
 mylist= [3,5,0,7,9]
 print("largest element is:", max(mylist))
+
+#3. (Как надо)
+a = input()
+b=0
+q = len(a)
+for i in range (1, q):
+  if int(a[i]) > int(b):
+    b=a[i]
+print(b)
