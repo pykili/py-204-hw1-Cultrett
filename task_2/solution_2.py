376443a1f3ecf37b646002d617802586591446bb
Cultrett <72413163+Cultrett@users.noreply.github.com>
2020-10-30 01:31:59 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -2,3 +2,17 @@
 user_input = input()
 # your code here
 print(most_frequent_character)
+
+
+user_input = input()
+most_frequent_character = ""
+max = 0
+for char in user_input:
+    count = 0
+    for current_char in user_input:
+        if current_char == char:
+            count = count + 1
+    if count > max:
+        max = count
+        most_frequent_character = char
+print(most_frequent_character)
